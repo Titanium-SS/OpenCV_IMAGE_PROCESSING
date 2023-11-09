f0701982b0534cbce89deeca61b4697b1d5e2080
Shailendra Singh <viperfury23@gmail.com>
2023-11-09 16:25:14 +0530

    Changed settings and ReadMe

--- 22_URLCHECKER.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-try:
-    page_url = "https://www.gettyimages.in/photos/india-empty-roads"
-    response = urllib.request.urlopen(page_url)
-    # process the response
-except urllib.error.HTTPError as e:
-    print(f"The server returned an HTTP error {e.code} - {e.reason}")
-    # handle the error
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # OpenCV_IMAGE_PROCESSING
+
+<hr>
+
+<center>
+<img src="readme\python.png" height=65>&nbsp;&nbsp;&nbsp;&nbsp;
+<img src="readme\opencv.webp" height=65>
+</center>
+
+### Contains Python 3.2 Codes for various functions available in the OpenCV Library of Python.
